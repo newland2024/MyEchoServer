9e76e4e01f837eeb21d0c05b74e90d9d9e969215
newland2024 <acb0y2010@gmail.com>
2024-10-31 13:05:09 +0800

    代码归档

--- BenchMark2/benchmark.cpp
+++ BenchMark2/benchmark.cpp
@@ -1,3 +1,110 @@
 #include <iostream>
+#include <string>
+#include <thread>
 
-int main() { return 0; }
+#include "common/cmdline.h"
+#include "common/epollctl.hpp"
+#include "EventDriven/eventloop.h"
+
+string ip;
+int64_t port;
+int64_t thread_count;
+int64_t pkt_size;
+int64_t client_count;
+int64_t run_time;
+int64_t max_req_count;
+int64_t rate_limit;
+bool is_debug;
+
+void usage() {
+  cout << "BenchMark -ip 0.0.0.0 -port 1688 -thread_count 1 -max_req_count "
+          "100000 -pkt_size 1024 -client_count 200 "
+          "-run_time 60 -rate_limit 10000 -debug" << endl;
+  cout << "options:" << endl;
+  cout << "    -h,--help                      print usage" << endl;
+  cout << "    -ip,--ip                       service listen ip" << endl;
+  cout << "    -port,--port                   service listen port" << endl;
+  cout << "    -thread_count,--thread_count   run thread count" << endl;
+  cout << "    -max_req_count,--max_req_count one connection max req count" << endl;
+  cout << "    -pkt_size,--pkt_size           size of send packet, unit is byte" << endl;
+  cout << "    -client_count,--client_count   count of client" << endl;
+  cout << "    -run_time,--run_time           run time, unit is second" << endl;
+  cout << "    -rate_limit,--rate_limit       rate limit, unit is qps/second" << endl;
+  cout << "    -debug,--debug                 debug mode, more info print" << endl;
+  cout << endl;
+}
+
+void ClientStart(EventDriven::EventLoop & event_loop) {
+  event_loop.TimerStart(1, ClientStart, std::ref(event_loop));
+}
+
+void Handler() {
+  EventDriven::EventLoop event_loop;
+  event_loop.TimerStart(1, ClientStart, std::ref(event_loop));
+  event_loop.Run();
+//   epoll_event events[2048];
+//   int epoll_fd = epoll_create(1);
+//   if (epoll_fd < 0) {
+//     perror("epoll_create failed");
+//     return;
+//   }
+//   Timer timer;
+//   bool is_running = true;
+//   std::string message(pkt_size + 1, 'a');
+//   ClientManager client_manager(ip, port, epoll_fd, &timer, client_count, message, max_req_count, is_debug, &is_running,
+//                                rate_limit, sum_stat, pct_stat);
+//   timer.Register(run_time * 1000, threadExit, std::ref(client_manager));
+//   timer.Register(1, clientManagerCheck, std::ref(client_manager));
+//   timer.Register(1000, rateLimitRefresh, std::ref(client_manager));  // 每秒刷新一下限流值
+//   while (is_running) {
+//     int64_t msec = 0;
+//     bool oneTimer = false;
+//     TimerData timer_data;
+//     oneTimer = timer.GetLastTimer(timer_data);
+//     if (oneTimer) {
+//       msec = timer.TimeOutMs(timer_data);
+//     }
+//     int num = epoll_wait(epoll_fd, events, 2048, msec);
+//     if (num < 0) {
+//       perror("epoll_wait failed");
+//       continue;
+//     } else if (num == 0) {
+//       sleep(0);  // 这里直接sleep(0)让出cpu。大概率被挂起，这里主动让出cpu，可以减少一次epoll_wait的调用
+//       msec = -1;  // 大概率被挂起，故这里超时时间设置为-1
+//     } else {
+//       msec = 0;  // 下次大概率还有事件，故msec设置为0
+//     }
+//     for (int i = 0; i < num; i++) {
+//       EchoClient *client = (EchoClient *)events[i].data.ptr;
+//       client->Deal();
+//     }
+//     if (oneTimer) timer.Run(timer_data);  // 处理定时器
+//   }
+}
+
+int main() {
+  CmdLine::StrOptRequired(&ip, "ip");
+  CmdLine::Int64OptRequired(&port, "port");
+  CmdLine::Int64OptRequired(&thread_count, "thread_count");
+  CmdLine::Int64OptRequired(&max_req_count, "max_req_count");
+  CmdLine::Int64OptRequired(&pkt_size, "pkt_size");
+  CmdLine::Int64OptRequired(&client_count, "client_count");
+  CmdLine::Int64OptRequired(&run_time, "run_time");
+  CmdLine::Int64OptRequired(&rate_limit, "rate_limit");
+  CmdLine::BoolOpt(&is_debug, "debug");
+  CmdLine::SetUsage(usage);
+  CmdLine::Parse(argc, argv);
+  thread_count = thread_count > 10 ? 10 : thread_count;
+  std::thread threads[10];
+//   constexpr key_t kShmKey = 12345; // 分配共享内存的key。
+//   SumStat sum_stat(kShmKey);
+//   PctStat pct_stat;
+  for (int64_t i = 0; i < thread_count; i++) {
+    threads[i] = std::thread(Handler);
+  }
+  for (int64_t i = 0; i < thread_count; i++) {
+    threads[i].join();
+  }
+//   printStatData(sum_stat, pct_stat);
+  return 0;
+}
