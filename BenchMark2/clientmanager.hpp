7350f29914ec5dcb876cabb8296980cbc716c392
newland2024 <acb0y2010@gmail.com>
2024-11-01 18:05:15 +0800

    代码归档

--- BenchMark2/clientmanager.hpp
+++ BenchMark2/clientmanager.hpp
@@ -8,14 +8,14 @@ namespace BenchMark2 {
 class ClientManager {
 public:
   ClientManager(MyCoroutine::Schedule &schedule, EventDriven::EventLoop &event_loop,
-                int64_t client_count, std::string ip, int port,
+                int64_t client_count, std::string ip, int port, std::string echo_message,
                 int64_t rate_limit)
       : schedule_(schedule), client_count_(client_count),
         rate_limit_(rate_limit), temp_rate_limit_(rate_limit) {
     clients_ = new Client *[client_count];
     for (int64_t i = 0; i < client_count_; i++) {
       clients_[i] =
-          new Client(schedule, event_loop, ip, port, temp_rate_limit_);
+          new Client(schedule, event_loop, ip, port, echo_message, temp_rate_limit_);
     }
   }
   ~ClientManager() {
